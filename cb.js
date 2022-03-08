58ed8cdbf7c00ba753103e2f3651e825321c2f77
Vinay G Shet <80429916+vinaygshet@users.noreply.github.com>
2022-03-08 21:41:31 +0530

    Add files via upload

---
+++ cb.js
@@ -0,0 +1,41 @@
+
+const t=1500;
+      
+setTimeout(()=>{
+    document.body.innerHTML=`<h1>10</h1>`
+    setTimeout(()=>{
+        document.body.innerHTML=`<h1>9</h1>`
+        setTimeout(()=>{
+            document.body.innerHTML=`<h1>8</h1>`
+            setTimeout(()=>{
+                document.body.innerHTML=`<h1>7</h1>`
+                setTimeout(()=>{
+                    document.body.innerHTML=`<h1>6</h1>`
+                    setTimeout(()=>{
+                        document.body.innerHTML=`<h1>5</h1>`
+                        setTimeout(()=>{
+                            document.body.innerHTML=`<h1>4</h1>`
+                            setTimeout(()=>{
+                                document.body.innerHTML=`<h1>3</h1>`
+                                setTimeout(()=>{
+                                    document.body.innerHTML=`<h1>2</h1>`
+                                    setTimeout(()=>{
+                                        document.body.innerHTML=`<h1>1</h1>`
+                                        setTimeout(()=>{
+                                            document.body.innerHTML=`<h1><span class="c1">Happy</span> <span class="c2">Independance</span> <span class="c1">day</span></h1>`
+                                        
+                                        },t);
+                                    },t);
+                                },t);
+                            },t);
+                        },t);
+                    },t);
+                },t);
+            },t);
+        },t);
+    },t);
+},t);
+
+
+
+
